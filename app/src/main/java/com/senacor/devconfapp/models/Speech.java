2f0663d63a722fd7f5313f22cb369608b13a9f44
s0549129 <s0549129@htw-berlin.de>
2016-11-14 14:01:05 +0100

    Speech Model - without startTime/endTime

---
+++ app/src/main/java/com/senacor/devconfapp/models/Speech.java
@@ -0,0 +1,83 @@
+package com.senacor.devconfapp.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Berlina on 14.11.16.
+ */
+
+public class Speech {
+
+    private String speechId;
+    private String speechTitle;
+    //private LocalTime startTime;
+    //private LocalTime endTime;
+    private String speechRoom;
+    private String speakerInfo;
+    private String speechSummary;
+    private String eventId;
+
+    public Speech(JSONObject object) {
+        try {
+            this.speechId = object.getString("speechId");
+            this.speechTitle = object.getString("speechTitle");
+            this.speechRoom = object.getString("speechRoom");
+            this.speakerInfo = object.getString("speakerInfo");
+            this.speechSummary = object.getString("speechSummary");
+            this.eventId = object.getString("eventId");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getSpeechId() {
+        return speechId;
+    }
+
+    public void setSpeechId(String speechId) {
+        this.speechId = speechId;
+    }
+
+    public String getSpeechTitle() {
+        return speechTitle;
+    }
+
+    public void setSpeechTitle(String speechTitle) {
+        this.speechTitle = speechTitle;
+    }
+
+    public String getSpeechRoom() {
+        return speechRoom;
+    }
+
+    public void setSpeechRoom(String speechRoom) {
+        this.speechRoom = speechRoom;
+    }
+
+    public String getSpeakerInfo() {
+        return speakerInfo;
+    }
+
+    public void setSpeakerInfo(String speakerInfo) {
+        this.speakerInfo = speakerInfo;
+    }
+
+    public String getSpeechSummary() {
+        return speechSummary;
+    }
+
+    public void setSpeechSummary(String speechSummary) {
+        this.speechSummary = speechSummary;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+}
