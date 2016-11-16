38d0772c7f29e5d57dab3199bf5119b20347aef0
s0549129 <s0549129@htw-berlin.de>
2016-11-16 11:27:54 +0100

    EventsActivity_add getSpeeches method (need to be tested - still excluded); layout list_speeches added listView

--- app/src/main/java/com/senacor/devconfapp/activities/EventsActivity.java
+++ app/src/main/java/com/senacor/devconfapp/activities/EventsActivity.java
@@ -6,32 +6,31 @@ import android.widget.ListView;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.senacor.devconfapp.adapters.EventAdapter;
+import com.senacor.devconfapp.adapters.SpeechAdapter;
 import com.senacor.devconfapp.models.Event;
 import com.senacor.devconfapp.R;
-
-
 import com.senacor.devconfapp.clients.EventRestClient;
-
-
+import com.senacor.devconfapp.models.Speech;
 import org.json.JSONArray;
 import org.json.JSONException;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.message.BasicHeader;
 
 public class EventsActivity extends AppCompatActivity {
 
     private ListView eventList;
+    private ListView speechList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        //setContentView(R.layout.list_speeches);
 
         getEvents();
+        //getSpeeches();
 
     }
 
@@ -43,14 +42,14 @@ public class EventsActivity extends AppCompatActivity {
 
     private void getEvents() {
 
-        List<Header> headers = new ArrayList<Header>();
+        List<Header> headers = new ArrayList<>();
         headers.add(new BasicHeader("Accept", "application/json"));
 
         EventRestClient.get(EventsActivity.this, "event/list", headers.toArray(new Header[headers.size()]),
                 null, new JsonHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                        ArrayList<Event> eventArray = new ArrayList<Event>();
+                        ArrayList<Event> eventArray = new ArrayList<>();
                         EventAdapter eventAdapter = new EventAdapter(EventsActivity.this, eventArray);
 
                         for (int i = 0; i < response.length(); i++) {
@@ -66,4 +65,32 @@ public class EventsActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    private void getSpeeches() {
+
+        List<Header> headers = new ArrayList<>();
+        headers.add(new BasicHeader("Accept", "application/json"));
+
+        EventRestClient.get(EventsActivity.this, "event/{eventID}/speeches", headers.toArray(new Header[headers.size()]),
+                null, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                        ArrayList<Speech> speechArray = new ArrayList<>();
+                        SpeechAdapter speechAdapter = new SpeechAdapter(EventsActivity.this, speechArray);
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                speechAdapter.add(new Speech(response.getJSONObject(i)));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        speechList= (ListView) findViewById(R.id.list_speeches);
+                        speechList.setAdapter(speechAdapter);
+                    }
+                });
+    }
+
 }
+
