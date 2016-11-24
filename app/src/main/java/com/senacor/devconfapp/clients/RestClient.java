6d76a51214446ec43a0dfdf033661d158283249f
matulja <maryna.fehler@gmail.com>
2016-11-24 22:58:31 +0100

    change date format to "HH:mm"

--- app/src/main/java/com/senacor/devconfapp/clients/RestClient.java
+++ app/src/main/java/com/senacor/devconfapp/clients/RestClient.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.senacor.devconfapp.IPAddress;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -14,7 +15,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class RestClient {
 
-   // private static final String BASE_URL = "http://"+ IPAddress.IP + ":8080/";
+   //private static final String BASE_URL = "http://"+ IPAddress.IP + ":8080/";
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
