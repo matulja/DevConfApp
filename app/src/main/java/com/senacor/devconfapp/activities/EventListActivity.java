d09f9e9b25fe99d6c30d0f4356da4e1be5e77737
s0549129 <s0549129@htw-berlin.de>
2016-11-30 14:32:55 +0100

    adapted Menu bar - added EventListActivity; shows ListAllEvents by clicking on menu item Show all events

---
+++ app/src/main/java/com/senacor/devconfapp/activities/EventListActivity.java
@@ -0,0 +1,85 @@
+package com.senacor.devconfapp.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.ListView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.senacor.devconfapp.IPAddress;
+import com.senacor.devconfapp.R;
+import com.senacor.devconfapp.adapters.EventListAdapter;
+import com.senacor.devconfapp.clients.RestClient;
+import com.senacor.devconfapp.models.Event;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.message.BasicHeader;
+
+import static com.senacor.devconfapp.R.layout.activity_events;
+
+/**
+ * Created by Berlina on 30.11.16.
+ */
+
+public class EventListActivity extends AppCompatActivity {
+
+    ListView eventList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_events);
+        getEventList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private void getEventList() {
+        List<Header> headers = new ArrayList<>();
+        headers.add(new BasicHeader("Accept", "application/json"));
+
+        RestClient.get(EventListActivity.this, IPAddress.IP + "/list", headers.toArray(new Header[headers.size()]),
+                null, new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                        System.out.println("in getEventList");
+                        ArrayList<Event> eventListArray = new ArrayList<>();
+                        EventListAdapter eventListAdapter = new EventListAdapter(EventListActivity.this, eventListArray);
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                eventListAdapter.add(new Event(response.getJSONObject(i)));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        eventList = (ListView) findViewById(R.id.list_events);
+                        eventList.setAdapter(eventListAdapter);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        Log.d("Failed: ", "" + statusCode);
+                        Log.d("Error : ", "" + throwable);
+                    }
+                    });
+    }
+}
