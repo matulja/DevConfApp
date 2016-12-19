636605b73e2031ba02481eb34dae957fecb5f5c7
Saba <nigistsaba@hotmail.com>
2016-12-19 22:17:56 +0100

    creates pop-up window for new speech

--- app/src/main/java/com/senacor/devconfapp/handlers/EventHandler.java
+++ app/src/main/java/com/senacor/devconfapp/handlers/EventHandler.java
@@ -1,22 +1,30 @@
 package com.senacor.devconfapp.handlers;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.senacor.devconfapp.IPAddress;
 import com.senacor.devconfapp.R;
+import com.senacor.devconfapp.activities.EventActivity;
+import com.senacor.devconfapp.adapters.EventListAdapter;
 import com.senacor.devconfapp.clients.AsynchRestClient;
-import com.senacor.devconfapp.listeners.ClickListener;
+import com.senacor.devconfapp.listeners.SpeechClickListener;
 import com.senacor.devconfapp.models.Event;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -31,6 +39,7 @@ public class EventHandler {
     private Event event;
     private Activity activity;
     SharedPreferences sharedPref;
+    ListView eventList;
 
     public EventHandler(Activity activity) {
         this.activity = activity;
@@ -58,12 +67,11 @@ public class EventHandler {
                         eventDate.setText(event.getDate());
 
                         ImageButton addSpeechButton = (ImageButton) activity.findViewById(R.id.addSpeechButton);
-                        addSpeechButton.setOnClickListener(new ClickListener(activity));
+
 
                         if (sharedPref.getString("role", "role").equals("ADMIN")) {
-                            LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                            View popupView = inflater.inflate(R.layout.popup_layout, null, false);
                             addSpeechButton.setVisibility(VISIBLE);
+                            addSpeechButton.setOnClickListener(new SpeechClickListener(activity));
                         }
 
 
@@ -80,11 +88,51 @@ public class EventHandler {
     }
 
 
-    public Event getEvent() {
-        return event;
+    public void getEventList(String url) {
+
+        AsynchRestClient.get(activity, url,
+                null, new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, final JSONArray response) {
+                        System.out.println("in getEventList");
+                        eventList = (ListView) activity.findViewById(R.id.list_events);
+                        ArrayList<Event> eventListArray = new ArrayList<>();
+                        EventListAdapter eventListAdapter = new EventListAdapter(activity, eventListArray);
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                Event event = new Event(response.getJSONObject(i));
+                                String id = response.getJSONObject(i).getString("eventId");
+                                event.setEventId(id);
+                                eventListAdapter.add(event);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        eventList.setAdapter(eventListAdapter);
+                        eventList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                Event event = (Event) eventList.getItemAtPosition(position);
+                                Intent intent = new Intent(activity,EventActivity.class);
+                                String url = IPAddress.IPevent+"/"+ event.getEventId();
+                                intent.putExtra("url", url);
+                                activity.startActivity(intent);
+                            }
+
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        Log.d("Failed: ", " cannot get all events ");
+                        Log.d("Failed: ", "" + statusCode);
+                        Log.d("Error : ", "" + throwable);
+                    }
+                });
     }
 
-    public void setEvent(Event event) {
-        this.event = event;
-    }
+
 }
