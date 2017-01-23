7e5a265c508471e540aecaf3c05eebeffa54a131
Veronika Babic <s0554747@htw-berin.de>
2017-01-23 13:28:22 +0100

    toolbar

--- app/src/main/java/com/senacor/devconfapp/activities/LoginActivity.java
+++ app/src/main/java/com/senacor/devconfapp/activities/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.senacor.devconfapp.activities;
 
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.loopj.android.http.RequestParams;
 import com.senacor.devconfapp.R;
@@ -19,6 +22,7 @@ import static com.senacor.devconfapp.R.id.username;
 public class LoginActivity extends AppCompatActivity {
 
     LogInOutHandler logInOutHandler;
+    Toolbar toolbar;
 
 
     @Override
@@ -26,6 +30,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         logInOutHandler = new LogInOutHandler(this);
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //AppLogo
+        getSupportActionBar().setLogo(R.mipmap.logow); //wrong logo diplayed
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        // Remove default title text
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        // Get access to the custom title view
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
 
         final EditText etUsername = (EditText) findViewById(username);
         final EditText etPassword = (EditText) findViewById(R.id.password);
